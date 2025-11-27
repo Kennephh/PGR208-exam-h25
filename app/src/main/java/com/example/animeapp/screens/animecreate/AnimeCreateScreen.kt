2c6ed815b20064ce4cd38f9554e217fd6da0e0cb
Michael Sasen Davidsen <md.vidsen@gmail.com>
2025-11-27 13:15:55 +0100

    Lagt til animeUserScreen

--- app/src/main/java/com/example/animeapp/screens/animecreate/AnimeCreateScreen.kt
+++ app/src/main/java/com/example/animeapp/screens/animecreate/AnimeCreateScreen.kt
@@ -1,24 +1,62 @@
 package com.example.animeapp.screens.animecreate
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import com.example.animeapp.data.database.Genre
+import com.example.animeapp.data.database.UserCreatedAnime
 
 @Composable
 fun AnimeCreateScreen(animeCreateViewModel: AnimeCreateViewModel){
-    val anime = animeCreateViewModel.userCreatedAnimeList.collectAsState()
+    val animeList by animeCreateViewModel.userCreatedAnimeList.collectAsState()
     animeCreateViewModel.setUserCreatedAnime()
 
     var newTitle by remember {
         mutableStateOf("")
     }
+    var genre by remember {
+        mutableStateOf("")
+    }
 
     Column{
+        Text("Add Anime")
 
+        TextField(
+            value = newTitle,
+            onValueChange = {newTitle = it},
+            label = {Text("Title")}
+        )
+
+        TextField(
+            value = genre,
+            onValueChange = {genre = it},
+            label = {Text("Genre")}
+        )
+
+        Button(
+            onClick = {
+                if(newTitle != null && genre != null){
+                    val newGenre = Genre(0,genre)
+                    val newAnime = UserCreatedAnime(name= newTitle, genre= newGenre)
+                    animeCreateViewModel.insertUserCreatedAnime(newAnime)
+                    newTitle = ""
+                    genre = ""
+                }
+            }
+        ) { Text("Add Anime") }
+    }
+    LazyColumn {
+        items(animeList) { anime ->
+            Text("Title: ${anime.name}, Genre: ${anime.genre}")
+        }
     }
 }
