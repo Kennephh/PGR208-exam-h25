03020525c26f4d41ac2585829796f1201c812544
Kennephh <150194783+Kennephh@users.noreply.github.com>
2025-11-27 12:13:31 +0100

    Endret return type på AnimeDao.kt, da denne returnerte List<UserCreatedAnime> og ikke UserCreatedAnime
    Begynt på LocalAnimeRepository.kt

--- app/src/main/java/com/example/animeapp/data/database/AnimeDao.kt
+++ app/src/main/java/com/example/animeapp/data/database/AnimeDao.kt
@@ -16,7 +16,7 @@ interface AnimeDao {
     suspend fun getAnimeById(id: Int) : UserCreatedAnime?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertNewAnime(animeList: List<UserCreatedAnime>)
+    suspend fun insertNewAnime(animeList: UserCreatedAnime)
 
     @Delete
     suspend fun deleteAnime(anime: UserCreatedAnime) : Int
