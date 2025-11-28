f62f548cd8707f59272ae77fa07e32689536b4c1
Kennephh <150194783+Kennephh@users.noreply.github.com>
2025-11-28 15:11:29 +0100

    AnimeCreateScreen.kt: * La setUserCreatedAnime inne i en LaunchedEffect slik at den kun kjøres én gang så lenge den skjermen vises. Dette for å slippe at den kalles ved hver endring brukeren gjør.
    * La til en "Hoved Column" som alt innholdet ligger inni. Slik at det ikke blir overlappende tekst fra tingene som ligger i DB
    
    MainActivity.kt:
    * Endret "this" til "applicationContext" slik initializeDatabase faktisk bruker Context som blir sendt til funksjonen. Er redd for minne-lekasje hvis ikke.

--- app/src/main/java/com/example/animeapp/screens/animecreate/AnimeCreateScreen.kt
+++ app/src/main/java/com/example/animeapp/screens/animecreate/AnimeCreateScreen.kt
@@ -1,24 +1,32 @@
 package com.example.animeapp.screens.animecreate
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.animeapp.data.database.Genre
 import com.example.animeapp.data.database.UserCreatedAnime
 
 @Composable
 fun AnimeCreateScreen(animeCreateViewModel: AnimeCreateViewModel){
     val animeList by animeCreateViewModel.userCreatedAnimeList.collectAsState()
-    animeCreateViewModel.setUserCreatedAnime()
+
+    LaunchedEffect(Unit) {
+        animeCreateViewModel.setUserCreatedAnime()
+    }
 
     var newTitle by remember {
         mutableStateOf("")
@@ -27,36 +35,50 @@ fun AnimeCreateScreen(animeCreateViewModel: AnimeCreateViewModel){
         mutableStateOf("")
     }
 
-    Column{
-        Text("Add Anime")
+    // Hoved Column
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(4.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(4.dp)
+        ) {
+            Text("Add Anime")
 
-        TextField(
-            value = newTitle,
-            onValueChange = {newTitle = it},
-            label = {Text("Title")}
-        )
+            TextField(
+                value = newTitle,
+                onValueChange = {newTitle = it},
+                label = {Text("Title")}
+            )
 
-        TextField(
-            value = genre,
-            onValueChange = {genre = it},
-            label = {Text("Genre")}
-        )
+            TextField(
+                value = genre,
+                onValueChange = {genre = it},
+                label = {Text("Genre")}
+            )
 
-        Button(
-            onClick = {
-                if(newTitle != null && genre != null){
-                    val newGenre = Genre(0,genre) // Alle har id: 0
-                    val newAnime = UserCreatedAnime(name= newTitle, genre= newGenre)
-                    animeCreateViewModel.insertUserCreatedAnime(newAnime)
-                    newTitle = ""
-                    genre = ""
+            Button(
+                onClick = {
+                    if(newTitle != null && genre != null){
+                        val newGenre = Genre(0,genre) // Alle har id: 0
+                        val newAnime = UserCreatedAnime(name= newTitle, genre= newGenre)
+                        animeCreateViewModel.insertUserCreatedAnime(newAnime)
+                        newTitle = ""
+                        genre = ""
+                    }
                 }
+            ) {
+
+                Text("Add Anime")
             }
-        ) { Text("Add Anime") }
-    }
-    LazyColumn {
-        items(animeList) { anime ->
-            Text("Title: ${anime.name}, Genre: ${anime.genre}")
+        }
+
+        LazyColumn(
+        ) {
+            items(animeList) { anime ->
+                Text("Title: ${anime.name}, Genre: ${anime.genre}")
+            }
         }
     }
 }
