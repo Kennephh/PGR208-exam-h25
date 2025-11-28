88859fa8ecc359190834d5900195d396c6b2e490
Alexander Toftesund <alexandertoftesund@gmail.com>
2025-11-28 22:22:25 +0100

    Update AnimeCreateScreen.kt

--- app/src/main/java/com/example/animeapp/screens/animecreate/AnimeCreateScreen.kt
+++ app/src/main/java/com/example/animeapp/screens/animecreate/AnimeCreateScreen.kt
@@ -1,27 +1,38 @@
 package com.example.animeapp.screens.animecreate
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.example.animeapp.data.database.Genre
 import com.example.animeapp.data.database.UserCreatedAnime
 
 @Composable
 fun AnimeCreateScreen(animeCreateViewModel: AnimeCreateViewModel){
+
     val animeList by animeCreateViewModel.userCreatedAnimeList.collectAsState()
 
     LaunchedEffect(Unit) {
@@ -35,53 +46,107 @@ fun AnimeCreateScreen(animeCreateViewModel: AnimeCreateViewModel){
         mutableStateOf("")
     }
 
-    // Hoved Column
+    fun addNewAnime(){
+        if(newTitle.isNotBlank() && genre.isNotBlank()){
+            val trimmedTitle = newTitle.trim()
+            val trimmedGenre = genre.trim()
+
+            val newGenre = Genre(0,trimmedGenre) // Alle har id: 0
+            val newAnime = UserCreatedAnime(name= trimmedTitle, genre= newGenre)
+            animeCreateViewModel.insertUserCreatedAnime(newAnime)
+            newTitle = ""
+            genre = ""
+        }
+    }
+
+    // Main start
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(4.dp)
     ) {
-        Column(
-            modifier = Modifier.padding(4.dp)
-        ) {
-            Text("Add Anime")
 
-            TextField(
+        // Title row start
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "Add new anime",
+                style = MaterialTheme.typography.titleSmall,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            HorizontalDivider(
+                thickness = 2.dp,
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .weight(1f)
+            )
+        } // Title row end
+
+        // Search bar start
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(start = 16.dp, bottom = 16.dp)
+                .fillMaxWidth()
+        ) {
+
+            OutlinedTextField(
                 value = newTitle,
                 onValueChange = {newTitle = it},
-                label = {Text("Title")}
+                label = {
+                    Text(
+                        text = "Enter title..",
+                        style = MaterialTheme.typography.labelMedium,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text
+                ),
+                keyboardActions = KeyboardActions(
+                    onSearch = {}
+                )
             )
 
-            TextField(
+            OutlinedTextField(
                 value = genre,
                 onValueChange = {genre = it},
-                label = {Text("Genre")}
+                label = {
+                    Text(
+                        text = "Enter genre..",
+                        style = MaterialTheme.typography.labelMedium,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text
+                ),
+                keyboardActions = KeyboardActions(
+                    onSearch = {}
+                )
             )
 
             Button(
-                onClick = {
-                    if(newTitle.isNotBlank() && genre.isNotBlank()){
-                        val trimmedTitle = newTitle.trim()
-                        val trimmedGenre = genre.trim()
-
-                        val newGenre = Genre(0,trimmedGenre) // Alle har id: 0
-                        val newAnime = UserCreatedAnime(name= trimmedTitle, genre= newGenre)
-                        animeCreateViewModel.insertUserCreatedAnime(newAnime)
-                        newTitle = ""
-                        genre = ""
-                    }
-                }
-            ) {
-
-                Text("Add Anime")
+                onClick = {addNewAnime()},
+            ){
+                Text("Add")
             }
-        }
+        } // Search bar end
 
-        LazyColumn(
-        ) {
+        LazyColumn() {
             items(animeList) { anime ->
                 Text("Title: ${anime.name}, Genre: ${anime.genre}")
             }
         }
-    }
+    } // Main end
 }
