6ed5d8c043517bc112649854e4c4a108d5ef86dd
Kennephh <150194783+Kennephh@users.noreply.github.com>
2025-11-28 15:40:09 +0100

    AnimeDetailsItem.kt: * Laget DetailsItem for mer info om anime når en bruker klikker på en anime

---
+++ app/src/main/java/com/example/animeapp/components/AnimeDetailsItem.kt
@@ -0,0 +1,12 @@
+package com.example.animeapp.components
+
+import androidx.compose.runtime.Composable
+import com.example.animeapp.data.api.Anime
+
+@Composable
+fun AnimeDetailsItem(
+    anime: Anime,
+    goBack: ( () -> Unit ) ? = null
+) {
+
+}
