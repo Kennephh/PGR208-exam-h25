765ad3fbd5ace3518478ebd4fe450fcd55f107bf
Alexander Toftesund <alexandertoftesund@gmail.com>
2025-11-29 15:34:40 +0100

    import cleanup

--- app/src/main/java/com/example/animeapp/components/AnimeItem.kt
+++ app/src/main/java/com/example/animeapp/components/AnimeItem.kt
@@ -1,6 +1,5 @@
 package com.example.animeapp.components
 
-import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
@@ -33,6 +32,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
