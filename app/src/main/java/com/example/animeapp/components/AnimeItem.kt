87a6f7db1fd948625124e832a2e60d3d0c2fa668
Michael Sasen Davidsen <md.vidsen@gmail.com>
2025-11-30 21:27:08 +0100

    Fjernet imports

--- app/src/main/java/com/example/animeapp/components/AnimeItem.kt
+++ app/src/main/java/com/example/animeapp/components/AnimeItem.kt
@@ -24,12 +24,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -41,8 +35,6 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.animeapp.data.api.Anime
-import com.example.animeapp.data.repository.LocalAnimeRepository
-import kotlinx.coroutines.launch
 
 @Composable
 fun AnimeItem(
--- app/src/main/java/com/example/animeapp/data/database/UserCreatedAnime.kt
+++ app/src/main/java/com/example/animeapp/data/database/UserCreatedAnime.kt
@@ -1,6 +1,5 @@
 package com.example.animeapp.data.database
 
-import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
