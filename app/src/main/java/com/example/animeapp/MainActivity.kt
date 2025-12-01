a38c188c53ef2c4ec5e815704427cc62a007528c
Michael Sasen Davidsen <md.vidsen@gmail.com>
2025-12-01 17:15:19 +0100

    Siste puss

--- app/src/main/java/com/example/animeapp/MainActivity.kt
+++ app/src/main/java/com/example/animeapp/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Sørger for at databasen starter først
         LocalAnimeRepository.initializeDatabase(this)
 
         enableEdgeToEdge()
