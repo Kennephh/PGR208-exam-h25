e84c08a412c361d47ce361bebad08e0ff1e435fc
Kennephh <150194783+Kennephh@users.noreply.github.com>
2025-12-01 12:11:17 +0100

    * Fjernet unødvendig linjeskift * Ryddet opp i MainActivity.kt: * Fjernet standard @Composable med fun Greeting()
    * Fjernet @Preview, @Composable og fun GreetingPreview

--- app/src/main/java/com/example/animeapp/MainActivity.kt
+++ app/src/main/java/com/example/animeapp/MainActivity.kt
@@ -5,10 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.animeapp.data.repository.LocalAnimeRepository
 import com.example.animeapp.navigation.AppNavigation
 import com.example.animeapp.screens.anime.AnimeListViewModel
@@ -28,9 +24,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         LocalAnimeRepository.initializeDatabase(this)
-
         enableEdgeToEdge()
         setContent {
             AnimeAPPTheme {
@@ -43,21 +37,5 @@ class MainActivity : ComponentActivity() {
                 )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AnimeAPPTheme {
-        Greeting("Android")
     }
 }
